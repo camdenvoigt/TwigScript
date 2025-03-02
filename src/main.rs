e99c2b96d726e5977e3a361b94c64d350c16f79e
Camden Voigt <camdenvoigt@gmail.com>
2025-03-02 11:49:48 -0800

    Switched to given pratt parser

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use pest::iterators::Pair;
+use pest::pratt_parser::PrattParser;
 use pest::Parser;
 use pest_derive::Parser;
 
@@ -11,55 +11,49 @@ enum Expression {
     Integer(i32),
     BinOp {
         lhs: Box<Expression>,
-        op: Op,
+        op: Operator,
         rhs: Box<Expression>,
     },
 }
 
 #[derive(Debug)]
-enum Op {
+enum Operator {
     Add,
     Subtract,
     Multiply,
     Divide,
 }
 
-fn parse_op_pair(pair: Pair<Rule>) -> Op {
-    match pair.as_rule() {
-        Rule::add => Op::Add,
-        Rule::subtract => Op::Subtract,
-        Rule::multiply => Op::Multiply,
-        Rule::divide => Op::Divide,
-        _ => panic!("Not Op type pair"),
-    }
-}
+fn parse_program() -> Expression {
+    let mut pairs = GrammarParser::parse(Rule::program, "5 * 5 + 10").unwrap();
+    use pest::pratt_parser::{Assoc::*, Op};
 
-fn parse_expression_pair(pair: Pair<'_, Rule>) -> Expression {
-    println!("Current pair: {}", pair.as_str());
-    match pair.as_rule() {
-        Rule::integer => Expression::Integer(pair.as_str().parse().unwrap()),
-        Rule::math_op => {
-            let mut inner_pairs = pair.into_inner();
+    let parser = PrattParser::new()
+        .op(Op::infix(Rule::add, Left) | Op::infix(Rule::subtract, Left))
+        .op(Op::infix(Rule::multiply, Left) | Op::infix(Rule::divide, Left));
+
+    let result = parser
+        .map_primary(|primary| match primary.as_rule() {
+            Rule::integer => Expression::Integer(primary.as_str().parse().unwrap()),
+            rule => unreachable!("Expected atomic rule found: {:?}", rule),
+        })
+        .map_infix(|lhs, op, rhs| {
+            let operator = match op.as_rule() {
+                Rule::add => Operator::Add,
+                Rule::subtract => Operator::Subtract,
+                Rule::multiply => Operator::Multiply,
+                Rule::divide => Operator::Divide,
+                rule => unreachable!("Expected Operator rule found: {:?}", rule),
+            };
             Expression::BinOp {
-                lhs: Box::new(parse_expression_pair(inner_pairs.next().unwrap())),
-                op: parse_op_pair(inner_pairs.next().unwrap()),
-                rhs: Box::new(parse_expression_pair(inner_pairs.next().unwrap())),
+                lhs: Box::new(lhs),
+                op: operator,
+                rhs: Box::new(rhs),
             }
-        }
-        Rule::EOI => Expression::Integer(0),
-        _ => unreachable!(),
-    }
-}
+        })
+        .parse(pairs.next().unwrap().into_inner());
 
-fn parse_program() -> Vec<Expression> {
-    let pairs = GrammarParser::parse(Rule::program, "5 * 5 + 10").unwrap();
-    let mut expression_trees = Vec::new();
-    println!("{:?}", pairs);
-    for pair in pairs {
-        expression_trees.push(parse_expression_pair(pair));
-    }
-    println!("{:?}", expression_trees);
-    expression_trees
+    result
 }
 
 fn interp_program(expr: Expression) -> i32 {
@@ -70,17 +64,17 @@ fn interp_program(expr: Expression) -> i32 {
             let right = interp_program(*rhs);
             println!("left: {}, right: {}", left, right);
             match op {
-                Op::Add => left + right,
-                Op::Subtract => left - right,
-                Op::Multiply => left * right,
-                Op::Divide => left / right,
+                Operator::Add => left + right,
+                Operator::Subtract => left - right,
+                Operator::Multiply => left * right,
+                Operator::Divide => left / right,
             }
         }
     }
 }
 
 fn main() {
-    let mut program = parse_program();
-    let result = interp_program(program.remove(0));
+    let program = parse_program();
+    let result = interp_program(program);
     println!("Result: {}", result);
 }
